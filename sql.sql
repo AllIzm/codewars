829390719a6e06b771faedd7e31606998f8162e8
Alena <121917310+AllIzm@users.noreply.github.com>
2023-08-03 09:57:16 +0300

    Delete sql.sql

--- sql.sql
+++
@@ -1,8 +0,0 @@
-/* 8 kyu SQL Basics: Mod  */
-SELECT MOD (number1, number2) FROM decimals;
-
-/* 8 kyu Find all active students */
-SELECT * FROM students WHERE IsActive = 1
-
-/* 8 kyu Adults only (SQL for Beginners #1) */ 
-SELECT * FROM users WHERE age >= 18;
