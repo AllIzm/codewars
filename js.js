6bbecb5ad79cdbf4e4d8d98c7cfe0adad83d720a
Alena <121917310+AllIzm@users.noreply.github.com>
2023-08-03 09:13:17 +0300

    add 8 kyu

--- js.js
+++ js.js
@@ -1,2 +1,13 @@
 // 8 kyu Up and down, the string grows
 const STRANGE_STRING = "ß"
+
+// 8 kyu Find Multiples of a Number
+function findMultiples(integer, limit) {
+  //your code here
+  const arr = [];
+  for(let i = integer; i <= limit; i += integer){
+    arr.push(i);
+  }
+  return arr;
+}
+
