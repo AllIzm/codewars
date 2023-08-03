e752ce8376ee086fc587e907a20679d533a93147
Alena <121917310+AllIzm@users.noreply.github.com>
2023-08-03 09:31:02 +0300

    update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,3 +3,6 @@ SELECT MOD (number1, number2) FROM decimals;
 
 /* 8 kyu Find all active students */
 SELECT * FROM students WHERE IsActive = 1
+
+/* 8 kyu Adults only (SQL for Beginners #1) */ 
+SELECT * FROM users WHERE age >= 18;
